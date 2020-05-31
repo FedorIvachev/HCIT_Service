4b0b8bd0f842635dd00bf082f8ea8fa5da5f7860
FedorIvachev <feodor.ivachev@gmail.com>
2020-05-31 22:01:08 +0300

    WeChat support added

---
+++ app/src/main/java/cn/edu/tsinghua/proxytalk/Wechat_12.java
@@ -0,0 +1,72 @@
+package cn.edu.tsinghua.proxytalk;
+
+import android.util.ArrayMap;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Map;
+
+import pcg.hcit_service.AccessibilityNodeInfoRecord;
+import pcg.hcit_service.MyExampleClass;
+
+// Transfer money select amount page
+public class Wechat_12 extends ActionDrivenLayout {
+    private static String GREETING = "朋友页面";
+    public static final String TAG  = "Wechat_Voice";
+
+    public Wechat_12(MyExampleClass context, String lowLevelPageName) {
+        super(context, lowLevelPageName);
+    }
+
+    @Override
+    public void onLoad() {
+        setThreshold(0.8f);
+        registerAction(new ITaskCallback<Result>() {
+            @Override
+            public void run(Result result) {
+                Map<String, String> paraValues = new ArrayMap<>();
+                switchPages("com.tencent.mm-1", paraValues);
+            }
+        }, "返回");
+
+        registerAction(new ITaskCallback<ActionDrivenLayout.Result>() {
+            @Override
+            public void run(ActionDrivenLayout.Result result) { //Called when the action is matched
+                Map<String, String> paraValues = new ArrayMap<>();
+                switchPages("com.tencent.mm-15", paraValues);
+            }
+        }, "发消息");
+
+        registerAction(new ITaskCallback<ActionDrivenLayout.Result>() {
+            @Override
+            public void run(ActionDrivenLayout.Result result) { //Called when the action is matched
+                Map<String, String> paraValues = new ArrayMap<>();
+                switchPages("com.tencent.mm-13", paraValues);
+            }
+        }, "音视频通话");
+
+        proxySpeak(GREETING, new ITaskCallback<String>() {
+            @Override
+            public void run(String result) {
+                listen();
+                Log.i(TAG, "Greeting success_12");
+            }
+        });
+    }
+
+    @Override
+    public void onChange(Map<String, List<AccessibilityNodeInfoRecord>> changeTypeToNodeList) {
+    }
+
+    @Override
+    public void onListenError(String message) {
+        System.err.println("An error has occurred when running voice recognition: " + message);
+        Log.i(TAG, "An error has occurred when running voice recognition: " + message);
+    }
+
+    @Override
+    public void onListenSuccess(String result) {
+        Log.i(TAG, "No error has occurred when running voice recognition: " + result);
+    }
+
+}
