cfe92f0de05c6146ab051853278066eb519664c7
FedorIvachev <feodor.ivachev@gmail.com>
2020-06-02 10:51:03 +0300

    remove file

--- app/src/main/java/cn/edu/tsinghua/proxytalk/AzureServices.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.tsinghua.proxytalk;
-
-public class AzureServices {
-
-    public static final String API_KEY_1 = "0a3110be28b3474aabafd4ee339a1cd3";
-    public static final String REGION = "eastus";
-
-}
